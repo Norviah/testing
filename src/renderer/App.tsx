5934608e5bdf8422973ebe725cfa0ce328790527
Norviah <21983700+Norviah@users.noreply.github.com>
2025-05-02 16:42:26 -0700

    2

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,28 +1,28 @@
-import electronLogo from './assets/electron.svg';
-import Versions from './components/Versions';
+import electronLogo from "./assets/electron.svg";
+import Versions from "./components/Versions";
 
 export function App(): React.JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping');
+  const ipcHandle = (): void => window.electron.ipcRenderer.send("ping");
 
   return (
     <>
-      <img alt='logo' className='logo' src={electronLogo} />
-      <div className='creator'>Powered by electron-vite</div>
-      <div className='text'>
-        <span className='ts'>App v1.0.0</span>
+      <img alt="logo" className="logo" src={electronLogo} />
+      <div className="creator">Powered by electron-vite</div>
+      <div className="text">
+        <span className="ts">App v1.0.0</span>
       </div>
-      <p className='tip'>
+      <p className="tip">
         Please try pressing <code>F12</code> to open the devTool
       </p>
-      <div className='actions'>
-        <div className='action'>
-          <a href='https://electron-vite.org/' target='_blank' rel='noreferrer'>
+      <div className="actions">
+        <div className="action">
+          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
             Documentation
           </a>
         </div>
-        <div className='action'>
+        <div className="action">
           {/* biome-ignore lint/a11y/useValidAnchor: */}
-          <a target='_blank' rel='noreferrer' onClick={ipcHandle}>
+          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
             Send IPC
           </a>
         </div>
